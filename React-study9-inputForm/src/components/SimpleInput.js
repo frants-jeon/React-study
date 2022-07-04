d4e27af0fab066d1e9d40c8bdfeeb60a93c89632
frants-jeon <jjh6963@naver.com>
2022-07-04 17:28:44 +0900

    single input valid check

--- React-study9-inputForm/src/components/SimpleInput.js
+++ React-study9-inputForm/src/components/SimpleInput.js
@@ -1,9 +1,48 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredInput, setEnteredInput] = useState("");
+  const [inputTouched, setInputTouched] = useState(false);
+
+  const inputIsValid = enteredInput.trim() !== "";
+
+  const inputChangeHandler = (event) => {
+    setEnteredInput(event.target.value);
+  };
+
+  const inputBlurHandler = () => {
+    setInputTouched(true);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    setInputTouched(true);
+
+    if (!inputIsValid) return;
+    console.log(enteredInput);
+    setEnteredInput("");
+    setInputTouched(false);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div
+        className={`form-control ${
+          inputTouched && !inputIsValid ? "invalid" : ""
+        }`}
+      >
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          value={enteredInput}
+          id="name"
+          onChange={inputChangeHandler}
+          onBlur={inputBlurHandler}
+        />
+        {inputTouched && !inputIsValid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
